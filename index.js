49ff9108837acc62ffe818719d76559d2d3b88fd
Mikhail Garbuzov <madmxg@gmail.com>
2017-05-28 23:04:08 +0300

    streams

--- index.js
+++ index.js
@@ -16,35 +16,75 @@ function closeDb (code = 0) {
     .then(() => process.exit(code));
 }
 
-return Promise.resolve()
-  .then(() => MongoClient.connect(mongoUrl))
-  .then((connectedDb) => {
-    log(`Connected to ${mongoUrl}`);
+function nextScrapParams (counter) {
+  const query = { _id: 'id' };
+  const update = { $inc: { counter: 1 } };
+  const options = { upsert: true, returnOriginal: false };
 
-    db = connectedDb;
-
-    const query = { _id: 'id' };
-    const update = { $inc: { counter: 1 } };
-    const options = { upsert: true, returnOriginal: false };
-    const counter = db.collection('counter');
-
-    return counter.findOneAndUpdate(query, update, options)
-      .then((result) => {
+  return counter.findOneAndUpdate(query, update, options)
+    .then((result) => {
         const P0 = 'pi';
         const P1 = 'ka';
         const P2 = 'bu';
-        const url = `http://${P0}${P1}${P2}.ru/story/_${result.value.counter}`;
 
-        return scraper(url)
-          .then((posts) => {
-            if (Array.isArray(posts)) {
-              return db.collection('posts').insertMany(posts);
-            }
-            return `No answer for ${url}`;
-          })
-          .then(result => log('Result', result.insertedCount || result));
-      });
+        const counter = result.value.counter;
+        const url = `http://${P0}${P1}${P2}.ru/story/_${counter}`;
 
+        return {
+          url,
+          counter,
+        };
+    });
+}
+
+function scrapStream (counter) {
+  return nextScrapParams(counter)
+    .then(params => {
+      return scraper(params.url)
+        .then((posts) => {
+          log(`Spawn ${params.url}`);
+
+          if (
+            Array.isArray(posts)
+            &&
+            posts.length
+          ) {
+            return posts;
+          }
+
+          log(`No answer for ${params.url}`);
+          return [];
+        });
+    });
+}
+
+return Promise.resolve()
+  .then(() => MongoClient.connect(mongoUrl))
+  .then((connectedDb) => {
+    db = connectedDb;
+
+    log(`Connected to ${mongoUrl}`);
+
+    const counter = db.collection('counter');
+
+    const parallel = [
+      scrapStream(counter),
+      scrapStream(counter),
+      scrapStream(counter),
+      scrapStream(counter),
+      scrapStream(counter)
+    ];
+
+    return Promise.all(parallel)
+      .then((all) => {
+        const mergedPosts = [].concat.apply([], all);
+
+        if (mergedPosts.length) {
+          return db.collection('posts').insertMany(mergedPosts);
+        }
+        return `No answer`;
+      })
+      .then(result => log('Result', result.insertedCount || result));
   })
   .then(() => closeDb())
   .catch(err => log('Error: ', err.stack || 'no stack', err))
